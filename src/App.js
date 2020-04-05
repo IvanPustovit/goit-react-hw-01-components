f7816e06e88756f29a86a4d0852fe4833fbd318a
IvanPustovit <pustovit.for@gmail.com>
2020-04-05 10:23:01 +0300

    ReactHom1_9

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Fragment } from "react";
-import Profile from "./Components/Profile/Profile";
+import React from "react";
+import Profile from "./components/Profile/Profile";
 import user from "./db/user.json";
-import Statistics from "./Components/Statistics/Statistics";
+import Statistics from "./components/Statistics/Statistics";
 import statisticalData from "./db/statistical-data.json";
-import FriendList from "./Components/FriendList/FriendList";
+import FriendList from "./components/FriendList/FriendList";
 import friendsData from "./db/friends.json";
-import TransactionHistory from "./Components/TransactionHistory/TransactionHistory";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import itemsData from "./db/transactions.json";
 
 const App = () => {
   return (
-    <Fragment>
+    <>
       <Profile user={user} />,
       <Statistics title="Upload stats" stats={statisticalData} />,
       <FriendList friends={friendsData} />
       <TransactionHistory items={itemsData} />
-    </Fragment>
+    </>
   );
 };
 
